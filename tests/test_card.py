e87691fda989f2d97e9bb040a8b14488f2e92261
Natali-Passero <Passero1vi77@proton.me>
2026-09-03 22:49:28 +0300

    add comment for test remove product from cart

--- tests/test_card.py
+++ tests/test_card.py
@@ -21,23 +21,25 @@ def test_add_product_to_cart(driver):
     assert cart_badge.text == "1" #проверить, что количество товаров равно 1
 
 
+
+#тест нужно оптимизировать, не самый лучший assert
 def test_remove_product_from_cart(driver):
     login(driver)
     driver.find_element(
         By.ID,
-        "add-to-cart-sauce-labs-backpack"
+        "add-to-cart-sauce-labs-backpack" #добавляю в корзину товар
     ).click()
     driver.find_element(
         By.ID,
-        "remove-sauce-labs-backpack"
+        "remove-sauce-labs-backpack"  #нахожу кнопку ремув
     ).click()
 
     remove_from_cart = driver.find_element(
         By.ID,
-        "add-to-cart-sauce-labs-backpack"
+        "add-to-cart-sauce-labs-backpack"  
         )
 
-    assert remove_from_cart.text == "Add to cart"
+    assert remove_from_cart.text == "Add to cart" #жду что вернется в исходное состояние
 
 
 
