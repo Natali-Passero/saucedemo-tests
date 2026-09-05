a2d5ce8a4d4a7fc345df22f0a039fda92a6cc398
Natali-Passero <Passero1vi77@proton.me>
2026-09-05 12:48:57 +0300

    fix test close eror button

--- tests/test_login.py
+++ tests/test_login.py
@@ -51,10 +51,14 @@ def test_empty_password(driver):
 def test_close_error_button(driver):
     login(driver, password="")
 
-    error_window = driver.find_element(
+    driver.find_element(
         By.CSS_SELECTOR,
-        '.svg-inline--fa.fa-xmark'
+        '[data-test="error"] .svg-inline--fa.fa-xmark'
     ).click()
 
-    assert len(driver.find_elements(By.CSS_SELECTOR, ".svg-inline--fa.fa-xmark")) == 0
+    error_window = driver.find_elements(
+        By.CSS_SELECTOR,
+        '[data-test="error"]'
+    )
 
+    assert not error_window
